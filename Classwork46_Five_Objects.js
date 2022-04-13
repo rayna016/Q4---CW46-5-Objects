581e20cd3867691e0608ee06ee1ec7ad8fb1163c
rayna016 <102683642+rayna016@users.noreply.github.com>
2022-04-13 11:46:01 -0500

    Add files via upload

---
+++ Classwork46_Five_Objects.js
@@ -0,0 +1,43 @@
+//- Create 5 Objects:
+//- Create a javascript file classwork46_Five_Objects.js
+//1. Car Object
+//2. House Object
+//3. Pizza Object
+//4. School Object
+//5. Story Object
+
+let car = {
+    type: "Honda",
+    color: "Blue",
+    numberOfDoors: "4",
+    hasSunroof: true,
+}
+
+let house = {
+    numberOfDoors: 10,
+    numberOfBathrooms: 2,
+    hasAgarage: true,
+}
+
+let pizza = {
+    hasPepperoni: false,
+    type: "Cheese",
+    pizzaShop: "Pizza Hut",
+    size: "Medium"
+}
+
+let school = {
+    floors: 2,
+    grades: "k-12",
+    city: "Irving",
+    hasCafeteria: true
+}
+
+let story = {
+    type: "hard copy",
+    name: "assassin's curse",
+    by: "Cassandra Rose Clarke",
+    color: "blue",
+    series: true
+
+}
